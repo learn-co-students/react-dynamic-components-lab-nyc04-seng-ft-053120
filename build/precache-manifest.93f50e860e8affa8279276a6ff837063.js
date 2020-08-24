d899953009fecd62ad384dfd7467ca5987c3ced2
tracycollins <tc@threeceemedia.com>
2020-08-23 22:26:56 -0400

    Done.

---
+++ build/precache-manifest.93f50e860e8affa8279276a6ff837063.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39dbaa2ec91c09bca22c",
+    "url": "/static/css/main.dcc9e403.chunk.css"
+  },
+  {
+    "revision": "39dbaa2ec91c09bca22c",
+    "url": "/static/js/main.febd2a03.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "e0af251b4e62487b3f7a044ffcd8c1b2",
+    "url": "/index.html"
+  }
+];
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,15 +4,17 @@ export default class ColorBox extends Component {
 
   state = {
     todos: [
-      
+
     ]
   }
 
   render() {
+    const newOpacity = this.props.opacity - 0.1;
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      this.props.opacity < 0.2 ? null : (<div className="color-box" style={{ opacity: this.props.opacity }}>
+        <ColorBox opacity={newOpacity} />
       </div>
+      )
     )
   }
 
